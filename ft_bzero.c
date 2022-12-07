55982ef1f173a1405efb3bcc0f5de8834ebc3c5e
Hasan Kemal ECE <109526257+ecehasankemal@users.noreply.github.com>
2022-12-07 12:11:28 +0300

    Add files via upload

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: hece  <hece@student.42kocaeli.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 14:51:45 by hece              #+#    #+#             */
-/*   Updated: 2022/09/18 14:52:52 by hece             ###   ########.tr       */
+/*   Updated: 2022/12/07 11:47:43 by hece             ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	ft_bzero(void *ptr, size_t bytelength)
 
 /*#include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putstr(char *str, int len)
 {
 	int	count;
 
 	count = 0;
-	while (str[count])
+	while (count < len)
 	{
 		write(1, &str[count], 1);
 		count++;
@@ -34,8 +34,8 @@ void	ft_putstr(char *str)
 int	main(void)
 {
 	char	a[] = "hece\n";
-	char	mete[] = "hece";
-	ft_putstr(a);
+	char	mete[] = "hece\n";
+	ft_putstr(a, 5);
 	ft_bzero(mete, 3);
-	ft_putstr(mete);
+	ft_putstr(mete, 5);
 }*/
